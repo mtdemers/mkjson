8dbf585a183aa0916d3bad8779cb824ea056ba9e
Michael DeMers <mtdemers@github.com>
2018-11-17 22:09:59 -0800

    Pep8 linting

--- mkjson.py
+++ mkjson.py
@@ -1,65 +1,75 @@
 """
 mkjson is designed to create json entries from a csv file
 it might not be glorious, but it will be all mine
+you must know the full filepath for the csv file
+the csv file must have an equal amount of headers and values (sequential)
 """
 
-def get_file():                                                                 #to get the file location
- print("Heyo, where's the fileo? ")
- file_name = raw_input()
 
- print("So you want me to make \'%s\' into a json file? (y/n)") % (file_name)   #verify that it's the right location
- acknowledge = raw_input()
- if acknowledge.lower() == 'y':
-     print("Alrighty, I'll do the thing!")
-     process_file(file_name)                                                    #run the processing
-     return
- elif acknowledge.lower() == 'n':
-     print("Ok, idk why you input \'%s\' then, but whatev...") % (file_name)
-     get_file()                                                                 #user indicated wrong file name, ask for new input
- else: 
-     print("Listen, bud, you gotta just type y or n.")
-     get_file()                                                                 #invalid input, ask for new input
+def get_file():  # to get the file location
+    print("Heyo, where's the fileo? ")
+    file_name = raw_input()
+
+    print("So you want me to make \'%s\' into a json file? (y/n)") \
+        % (file_name)  # verify that it's the right location
+    acknowledge = raw_input()
+    if acknowledge.lower() == 'y':
+        print("Alrighty, I'll do the thing!")
+        process_file(file_name)  # run the processing
+        return
+    elif acknowledge.lower() == 'n':
+        print("Ok, idk why you input \'%s\' then, but whatev...") % (file_name)
+        get_file()  # user indicated wrong file name, ask for new input
+    else:
+        print("Listen, bud, you gotta just type y or n.")
+        get_file()  # invalid input, ask for new input
+
 
 def process_file(file):
     print ("opening \'%s\'") % (file)
     info = []
     f = open(file, "r")
-    for line in f:                                                              #iterate through lines
+    for line in f:  # iterate through lines
         info.append(line)
-    headers = info[0]                                                           #separate first line into headers
-    keys = []                                                                   #blank list for parsed headers into keys
-    sep = ','                                                                   #comma delimiter
-    nl = '\r\n'                                                                 #new line delimiter
+    headers = info[0]  # separate first line into headers
+    keys = []  # blank list for parsed headers into keys
+    sep = ','  # comma delimiter
+    nl = '\r\n'  # new line delimiter
 
-    for word in headers.split(sep,):                                            #separate first row as headers
+    for word in headers.split(sep,):  # separate first row as headers
         if nl in word:
-            head = word.split(nl,1)[0]                                          #remove \r\n newline indicators
+            head = word.split(nl, 1)[0]  # remove \r\n newline indicators
             keys.append(head)
         else:
             keys.append(word)
-    
+
     info.pop(0)
-    values = []                                                                 #blank list for parsed values
-    for line in info:                                                           #iterate through lines
-        for word in line.split(sep,):                                           #iterate through words using delimiter
+    values = []  # blank list for parsed values
+    for line in info:  # iterate through lines
+        for word in line.split(sep,):  # iterate through words using delimiter
             if nl in word:
-                head = word.split(nl,1)[0]                                      #separate new line delimiter
-                values.append(head)                                             #append word without delimiter
+                head = word.split(nl, 1)[0]  # separate new line delimiter
+                values.append(head)  # append word without delimiter
             else:
                 values.append(word)
 
     count = 0
-    for word in values:                                                         #combine keys and values accordingly
-        if count == 0:                                                          #if first item in a row add open curly bracket
-            print "{" + ('"%s": "%s",') % (keys[(values.index(word) % len(keys))],values[values.index(word)])
+    for word in values:  # combine keys and values accordingly
+        if count == 0:  # if first item in a row add open curly bracket
+            print "{" + ('"%s": "%s",') % \
+                (keys[(values.index(word) % len(keys))],
+                 values[values.index(word)])
             count += 1
-        elif count == (len(keys) - 1):                                          #if last item in a row add closed curly bracket
-            print ('"%s": "%s"') % (keys[(values.index(word) % len(keys))],values[values.index(word)]) + " }"
+        elif count == (len(keys) - 1):  # add closed curly for last item
+            print ('"%s": "%s"') % \
+                (keys[(values.index(word) % len(keys))],
+                 values[values.index(word)]) + " }"
             count = 0
-        else:                                                                   #if in middle of a row no bracket
-            print ('"%s": "%s",') % (keys[(values.index(word) % len(keys))],values[values.index(word)])
-            count += 1        
+        else:  # if in middle of a row no bracket
+            print ('"%s": "%s",') % \
+                (keys[(values.index(word) % len(keys))]
+                 values[values.index(word)])
+            count += 1
 
     return
 get_file()
-
